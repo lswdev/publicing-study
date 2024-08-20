229d50844be353f3e27d003d549ca850f8e1941b
lswsepia <lswsepia@gmail.com>
2024-08-20 10:03:20 +0900

    퍼블리싱 테스트 코드 올려드립니다.

---
+++ test/js/js-test01.js
@@ -0,0 +1,18 @@
+
+const jsonData = {
+  title : 'JSON 데이터 가져오기',
+  userName : '쥐드래곤',
+  userAuth : 'admin',
+  userDept : 'DX연구소',
+  userSkill : {
+    html : '상급',
+    css : '중급',
+    javascript : '하급'
+  },
+  userId : 'kimBob',
+}
+// 위 데이터를 이용해 화면에 출력하세요.
+
+
+
+
